f02ea2162bf892b23cdda884b5d9d6262328b095
Gyrotron <simmarkalsi@gmail.com>
2018-06-05 22:45:11 +0530

    Test

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Simple todo list app
 1. adb connect <ip>:5555
 2. react-native start
 3. react-natice run-android/run-ios
+4. npm run android-windows
---
+++ app/components/Main.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  TouchableOpacity
+} from 'react-native';
+
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View><Text>Test</Text></View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    
+  },
+});
